ca52521939b56c1c98f8ed32a2195a4bfaf8afcc
Zayn Jarvis <ZaynJarvis@gmail.com>
2018-12-07 10:35:47 +0800

    add useless frontend

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const rp = require("request-promise");
 const $ = require("cheerio");
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("This is an app for Flutter Pub Package Badge generatiion.");
+});
 app.get("/version/:name", async (req, res) => {
   const html = await rp(`https://img.shields.io/pub/v/${req.params.name}.svg`);
   res.send(html);
@@ -11,7 +14,7 @@ app.get("/version/:name", async (req, res) => {
 app.get("/score/:name", async (req, res) => {
   const html = await rp(`https://pub.dartlang.org/packages/${req.params.name}`);
 
-  $(".score-box > span", html).each(async function() {
+  $(".score-box > .number", html).each(async function() {
     const badge = await rp(
       `https://img.shields.io/badge/pub.score-${$(this).text()}-blue.svg`
     );
